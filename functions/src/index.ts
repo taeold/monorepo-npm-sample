1f1729c48d925d1a5f4023671545b99ace04849b
Daniel Young Lee <taeold@gmail.com>
2023-06-14 11:52:38 -0700

    Init commit.

---
+++ functions/src/index.ts
@@ -0,0 +1,18 @@
+/**
+ * Import function triggers from their respective submodules:
+ *
+ * import {onCall} from "firebase-functions/v2/https";
+ * import {onDocumentWritten} from "firebase-functions/v2/firestore";
+ *
+ * See a full list of supported triggers at https://firebase.google.com/docs/functions
+ */
+
+import {onRequest} from "firebase-functions/v2/https";
+import * as logger from "firebase-functions/logger";
+// @ts-ignore
+import { MAGIC_STRING } from "@danielylee/shared";
+
+export const helloWorld = onRequest((request, response) => {
+  logger.info("Hello logs!", {structuredData: true});
+  response.send(MAGIC_STRING);
+});
---
+++ shared/index.js
@@ -0,0 +1,1 @@
+exports.MAGIC_STRING = "magic 🪄";
